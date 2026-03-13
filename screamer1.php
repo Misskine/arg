368a05fd57a0b4f475df69c86d9cb9114cc270e6
Anissa Dahabi <anissa.Dahabi@gmail.com>
2026-03-13 16:50:02 +0100

    yeux

---
+++ screamer1.php
@@ -0,0 +1,13 @@
+<img class="img" src="images/image.png">
+
+<style>
+    body {
+        height: 100%;
+        background-color: black;
+        overflow: hidden;
+    }
+
+    .img {
+        height: 100%;
+    }
+</style>
